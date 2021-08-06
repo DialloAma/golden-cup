ca52d2b20b7104275b74dc4abcf55016e0b9c26c
DIALLO <amadou9393@yahoo.fr>
2021-08-06 02:50:17 +0000

    install react router dom

---
+++ src/Components/Route.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {} from 'react-router-dom'
+
+const Route = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Route;
